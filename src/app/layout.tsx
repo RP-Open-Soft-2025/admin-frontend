7db26f4f148483ca34093873c504580154c6edc9
Bikram Ghuku <bikramghuku05@gmail.com>
2025-04-01 22:27:50 +0530

    fix: toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import { SidebarProvider } from '@/components/context/SidebarContext'
 import { ThemeProvider } from '@/components/context/ThemeContext'
 import StateProvider from '@/components/context/ReduxContext'
+import { Toaster } from 'sonner'
 
 const outfit = Outfit({
 	variable: '--font-outfit-sans',
@@ -23,6 +24,7 @@ export default function RootLayout({
 				<ThemeProvider>
 					<StateProvider>
 						<SidebarProvider>{children}</SidebarProvider>
+						<Toaster />
 					</StateProvider>
 				</ThemeProvider>
 			</body>
